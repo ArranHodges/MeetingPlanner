01269965591116dfb8eaedb5c401fa0e2715d5e0
ArranHodges <arran_hodges@hotmail.com>
2016-03-21 01:03:38 +0000

    Sql

--- Database/createdbmeeting.sql
+++ Database/createdbmeeting.sql
@@ -4,18 +4,26 @@ CREATE DATABASE dbMeeting;
 USE dbMeeting;
 
 # Create User table
-CREATE TABLE Customer (
-	UserID integer NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+CREATE TABLE Username (
+	userID integer NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	firstName varchar(255) NOT NULL, 
 	lastName varchar(255) NOT NULL, 
-	dateOfBirth date NOT NULL, 
 	emailAddress varchar(255) NOT NULL, 
-	mobileNo varchar(11) NOT NULL, 
-	customerBranchID integer NOT NULL, 
-	address integer NOT NULL,
-	consignments integer NOT NULL,
-	FOREIGN KEY (customerBranchID) REFERENCES Branch(branchID),
-	FOREIGN KEY (address) REFERENCES Address(addressID)
-	# Can't add this because it isnt primary nor unique
-	# FOREIGN KEY (consignments) REFERENCES CustomerConsignments(consignmentsID)
+) ENGINE=InnoDB;
+
+# Create MeetingData table
+CREATE TABLE MeetingData (
+	meetingID integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	usersID integer,
+	title varchar(255) NOT NULL, 
+	Description varchar(255) NOT NULL, 
+	emailAddress varchar(255) NOT NULL, 
+) ENGINE=InnoDB;
+
+# Create MeetingAttendee table
+CREATE TABLE  MeetingAttendee (
+	userID integer NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	firstName varchar(255) NOT NULL, 
+	lastName varchar(255) NOT NULL, 
+	emailAddress varchar(255) NOT NULL, 
 ) ENGINE=InnoDB;
